4f671c2c7af3d961b729c85e8bcfe4c3ed1ad810
Chris Allen <cma@bitemyapp.com>
2017-11-02 19:53:56 -0500

    Initial checkers

---
+++ tests/tests.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Control.Applicative
+import           Data.Either.Validation
+import           Data.Functor (void)
+import           Data.Monoid (Sum(..))
+
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import Hedgehog.Checkers
+
+genValidation :: Gen a -> Gen b -> Gen (Validation a b)
+genValidation ga gb = do
+  a <- ga
+  b <- gb
+  Gen.choice [return $ Failure a, return $ Success b]
+
+validationAlternative :: Property
+validationAlternative = property $ do
+  let genSumInt = Sum <$> Gen.int (Range.linear 0 maxBound)
+      genVal = genValidation genSumInt genSumInt
+  alternative genVal
+
+genInt = Gen.int (Range.linear 0 maxBound)
+genEither' :: Gen a -> Gen b -> Gen (Either a b)
+genEither' ga gb = do
+  a <- ga
+  b <- gb
+  Gen.choice [return $ Left a, return $ Right b]
+genEither = genEither' genInt genInt
+
+eitherAlt :: Property
+eitherAlt = property $ do
+  alt genEither
+
+eitherBifunctor :: Property
+eitherBifunctor = property $ do
+  bifunctor genEither genInt genInt genInt
+
+eitherFunctor :: Property
+eitherFunctor = property $ do      
+  functor genEither
+
+eitherApplicative :: Property
+eitherApplicative = property $ do
+  applicative genEither genInt genInt genInt
+
+eitherSemigroup :: Property
+eitherSemigroup = property $ do
+  semigroup genEither
+
+main :: IO ()
+main = do
+  void $
+    checkParallel $
+      Group "Data.Either" [ ("Alt", eitherAlt)
+                          , ("Bifunctor", eitherBifunctor)
+                          , ("Functor", eitherFunctor)
+                          , ("Semigroup", eitherSemigroup)
+                          , ("Applicative", eitherApplicative)
+                          ]
